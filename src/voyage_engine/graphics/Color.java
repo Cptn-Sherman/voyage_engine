3697e600a4d8461143f903e4c1e41da975f714bd
athom203 <Cptn-Sherman@users.noreply.github.com>
2021-02-20 21:23:00 -0500

    added support to generate color with ints instead of floats.

--- src/voyage_engine/graphics/Color.java
+++ src/voyage_engine/graphics/Color.java
@@ -10,8 +10,18 @@ public class Color {
     public static final Color BLUE = new Color(0.0f, 0.0f, 1.0f, 1.0f);
     
     Vec4 color_as_vector;
+    public int r, g, b, a;
+    
     public Color (float r, float g, float b, float a) {
         color_as_vector = new Vec4(r, g, b, a);
+    }
+
+    public Color (int r, int g, int b, int a) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        this.a = a;
+        color_as_vector = new Vec4(r / 255f, g / 255f, b / 255f, a / 255f) ;
     }
 
     public Vec4 getColorVector() {
